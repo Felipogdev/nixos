c331ef1f2e5c51377b57ee89a849a2ea9c3a2bf3
Felipogdev <felipedantaslaurentinodev@gmail.com>
2025-06-16 12:26:02 +0100

    tlp config

--- configuration.nix
+++ configuration.nix
@@ -88,7 +88,24 @@
 
 ];
 
-  services.tlp.enable = true;
+  services.tlp = {
+  enable = true;
+  settings =  {
+     CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+     CPU_SCALING_GOVERNOR_ON_BAR = "powersave";
+
+     CPU_ENERGY_PERF_POLICY_ON_BAR = "power";
+     CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+     CPU_MIN_PERF_ON_AC = 0;
+     CPU_MAX_PERF_ON_AC= 100;
+     CPU_MIN_PERF_ON_BAT = 0;
+     CPU_MAX_PERF_ON_BAR = 20;
+
+     START_CHARGE_THRESH_BAT0 = 40;
+     STOP_CHARGE_THRESH_BAT0= 80;
+   };
+  };
 
   # DNS
   services.resolved.enable = true;
